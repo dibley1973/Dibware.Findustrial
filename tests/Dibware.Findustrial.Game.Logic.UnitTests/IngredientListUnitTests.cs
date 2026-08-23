6e283683f914c9ccb64277e1ac40c9b582fecbaa
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-23 21:34:50 +0100

    Add the satrt of `IngredientListUnitTests`

---
+++ tests/Dibware.Findustrial.Game.Logic.UnitTests/IngredientListUnitTests.cs
@@ -0,0 +1,85 @@
+namespace Dibware.Findustrial.Game.Logic.UnitTests;
+
+using Dibware.Findustrial.Game.Logic.BluePrint;
+using Dibware.Findustrial.Game.Logic.Entities;
+using Dibware.Findustrial.Game.Logic.Quantities;
+
+/// <summary>
+/// Unit tests for the <see cref="IngredientList"/> class.
+/// </summary>
+[TestClass]
+public sealed class IngredientListUnitTests
+{
+    [TestMethod]
+    public void All_AfterConstruction_ReturnsEmpty()
+    {
+        // Arrange
+        var list = new IngredientList();
+
+        // Act
+        var actual = list.All();
+
+        // Assert
+        Assert.IsNotNull(actual);
+        Assert.AreEqual(0, actual.Count);
+    }
+
+    [TestMethod]
+    public void All_AfterAddingMaterialTypeWithQuantityOfOne_ReturnsThatItem()
+    {
+        // Arrange
+        var list = new IngredientList();
+        var material = new Material("Iron");
+        var materialQuantity = new MaterialQuantity(material, 1);
+
+        list.Add(materialQuantity);
+
+        // Act
+        var actual = list.All();
+
+        // Assert
+        Assert.IsNotNull(actual);
+        Assert.AreEqual(1, actual.Count);
+        Assert.AreEqual(actual.Keys.First().Name, material.Name);
+        Assert.AreEqual(actual.Values.First(), 1);
+    }
+
+    [TestMethod]
+    public void All_AfterAddingMaterialTypeWithQuantityOfTwo_ReturnsThatItem()
+    {
+        // Arrange
+        var list = new IngredientList();
+        var material = new Material("Iron");
+        var materialQuantity = new MaterialQuantity(material, 2);
+
+        list.Add(materialQuantity);
+
+        // Act
+        var actual = list.All();
+
+        // Assert
+        Assert.IsNotNull(actual);
+        Assert.AreEqual(1, actual.Count);
+        Assert.AreEqual(actual.Keys.First().Name, material.Name);
+        Assert.AreEqual(actual.Values.First(), 2);
+    }
+
+    [TestMethod]
+    public void Add_AddingTwoIdenticalMaterials_ThrowsException()
+    {
+        // Arrange
+        var list = new IngredientList();
+        var material1 = new Material("Iron");
+        var material2 = new Material("Iron");
+        var materialQuantity1 = new MaterialQuantity(material1, 2);
+        var materialQuantity2 = new MaterialQuantity(material2, 2);
+
+        list.Add(materialQuantity1);
+
+        // Act
+        Action actual = () => list.Add(materialQuantity2);
+
+        // Assert
+        Assert.ThrowsException<InvalidOperationException>(actual);
+    }
+}
