3b7b65a12803725f6dd3d945e4aa2ac1b518618b
Dibley1973 <dibley1973@yahoo.co.uk>
2026-08-24 21:57:28 +0100

    Add `ThrowIfTrue()` to `SameMaterialAlreadyAddedException`

--- src/Dibware.Findustrial.Game.Logic/Exceptions/SameMaterialAlreadyAddedException.cs
+++ src/Dibware.Findustrial.Game.Logic/Exceptions/SameMaterialAlreadyAddedException.cs
@@ -15,6 +15,12 @@ public class SameMaterialAlreadyAddedException : Exception
     /// </summary>
     public SameMaterialAlreadyAddedException() : base() { }
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="SameMaterialAlreadyAddedException"/> class.
+    /// </summary>
+    /// <param name="material">
+    /// A reference to the material which is already added.
+    /// </param>
     public SameMaterialAlreadyAddedException(Material material)
         : base($"Material '{material.Name}' has already been added. Please update the existing material instead.")
     { }
@@ -24,4 +30,19 @@ public class SameMaterialAlreadyAddedException : Exception
     /// </summary>
     /// <param name="message">The message that describes the error.</param>
     public SameMaterialAlreadyAddedException(string message) : base(message) { }
+
+    /// <summary>
+    /// Throws a new <see cref="SameMaterialAlreadyAddedException"/> with the specified <paramref name="message"/>
+    /// if the specified <paramref name="condition"/> is <see langword="true"/>.
+    /// </summary>
+    /// <param name="condition">The condition to check.</param>
+    /// <param name="material">
+    /// A reference to the material which is already added.
+    /// </param>
+    /// <exception cref="SameMaterialAlreadyAddedException"></exception>
+    public static void ThrowIfTrue(bool condition, Material material)
+    {
+        if (condition)
+            throw new SameMaterialAlreadyAddedException(material);
+    }
 }
